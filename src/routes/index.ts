2d212d32b594cdd1cec7b7298d560b3ad20b6fd4
Raniere Rodrigues Gomes <raniere57@icloud.com>
2022-11-01 20:32:56 -0300

    criando boilerplate node

---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+/* eslint-disable new-cap */
+import {Router} from 'express';
+
+const routes = Router();
+
+routes.get('/', (req, res) => res.json({message: 'eaeaeewww'}));
+
+export default routes;
+
